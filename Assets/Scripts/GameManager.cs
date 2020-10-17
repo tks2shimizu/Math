363d248bdfd75e9a990d1100da6eb537b448bc8f
Tomoaki Shimizu <tomoaki.shimizu@tks2.co.jp>
2020-10-17 11:19:57 +0900

    ベース

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private enum Type
+    {
+        Type1,
+        Type2,
+        Type3,
+    }
+
+    [SerializeField]
+    private Type type = Type.Type1;
+    private GameObject _sumomo;
+    private Vector3 _sumomoStartPosition;
+    private Vector3 _position = new Vector3(0, 0, 0);
+    private const float Range = 150f;
+
+    void Start()
+    {
+        _sumomo = GameObject.Find("Sumomo");
+        _sumomoStartPosition = _sumomo.transform.position;
+    }
+
+    public void PointDownSumomo()
+    {
+        _position = Input.mousePosition;
+    }
+    
+    public void DragSumomo()
+    {
+        var diff = _position - Input.mousePosition;
+
+        _sumomo.transform.position -= diff;
+
+        if (type == Type.Type2)
+        {
+            //your code
+        }
+        else if (type == Type.Type3)
+        {
+            //your code
+        }
+        
+        _position = Input.mousePosition;
+    }
+}
